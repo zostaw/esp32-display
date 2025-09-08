8d26c1abe30c7351ae83801725923c7c843ece0c
zostaw <zostaw@users.noreply.github.com>
2025-09-09 00:12:40 +0200

    markov

--- esp32-display.ino
+++ esp32-display.ino
@@ -45,23 +45,34 @@ TFT_eSPI tft = TFT_eSPI();
 uint16_t main_color = 0xF805;
 
 // The number of states in the Markov chain.
-const int NUM_STATES = 5;
+const int NUM_STATES = 16;
 
 
 // The transition probability matrix for the Markov chain.
 // P(i, j) is the probability of moving from state `i` to state `j`.
 // Corresponds to Racket's `markov-probability-matrix`.
-const float markov_probability_matrix[NUM_STATES][NUM_STATES] = {
-  {0.3231, 0.1078, 0.2390, 0.1901, 0.1400},
-  {0.0023, 0.7456, 0.1201, 0.0820, 0.0500},
-  {0.2000, 0.2500, 0.3500, 0.0999, 0.1001},
-  {0.1111, 0.2222, 0.3333, 0.2223, 0.1111},
-  {0.0501, 0.0502, 0.0503, 0.0504, 0.7990}
+const float markov_probability_matrix[16][16] = {
+  {0.1127, 0.0204, 0.0274, 0.0421, 0.0745, 0.0539, 0.0539, 0.1007, 0.0053, 0.0713, 0.1124, 0.0948, 0.0011, 0.1132, 0.1039, 0.0124},
+  {0.0345, 0.0162, 0.0314, 0.0664, 0.1089, 0.1232, 0.0535, 0.0283, 0.0847, 0.0440, 0.0746, 0.1036, 0.1098, 0.0642, 0.0143, 0.0424},
+  {0.1054, 0.0151, 0.0247, 0.0842, 0.0210, 0.0636, 0.0999, 0.0737, 0.0346, 0.1287, 0.1009, 0.0284, 0.0503, 0.0795, 0.0292, 0.0608},
+  {0.0973, 0.0783, 0.0800, 0.0282, 0.0488, 0.0382, 0.0601, 0.0104, 0.0685, 0.0891, 0.0864, 0.0946, 0.0385, 0.0580, 0.0653, 0.0583},
+  {0.1067, 0.0259, 0.0931, 0.1049, 0.0519, 0.0651, 0.0512, 0.0927, 0.0608, 0.0264, 0.0611, 0.1057, 0.1043, 0.0196, 0.0079, 0.0227},
+  {0.0194, 0.1222, 0.0670, 0.1040, 0.0335, 0.1212, 0.0685, 0.0674, 0.0565, 0.0270, 0.0067, 0.0431, 0.1030, 0.0294, 0.0605, 0.0706},
+  {0.0282, 0.0303, 0.0157, 0.0626, 0.0752, 0.0874, 0.0595, 0.0672, 0.1004, 0.0707, 0.0958, 0.1012, 0.0410, 0.0870, 0.0685, 0.0093},
+  {0.0211, 0.0085, 0.0849, 0.0140, 0.0548, 0.0841, 0.1054, 0.0247, 0.0670, 0.1084, 0.0257, 0.1005, 0.0801, 0.0884, 0.0999, 0.0325},
+  {0.0318, 0.0945, 0.0881, 0.0415, 0.1049, 0.0280, 0.0278, 0.1100, 0.0424, 0.0849, 0.0135, 0.0680, 0.0260, 0.0968, 0.0517, 0.0901},
+  {0.0565, 0.1046, 0.0727, 0.1021, 0.0078, 0.0462, 0.0357, 0.0920, 0.0868, 0.0473, 0.0675, 0.1042, 0.0206, 0.0794, 0.0696, 0.0070},
+  {0.1004, 0.1107, 0.0436, 0.1154, 0.0169, 0.0433, 0.0776, 0.0528, 0.0428, 0.0790, 0.0306, 0.1363, 0.0132, 0.1133, 0.0075, 0.0166},
+  {0.0291, 0.1536, 0.0678, 0.0054, 0.0067, 0.0307, 0.0894, 0.0452, 0.0117, 0.0514, 0.0531, 0.1251, 0.1043, 0.0740, 0.0226, 0.1299},
+  {0.0543, 0.0330, 0.0644, 0.0279, 0.1010, 0.1110, 0.0682, 0.0175, 0.0267, 0.1112, 0.1036, 0.0515, 0.0140, 0.0965, 0.0922, 0.0270},
+  {0.1474, 0.0211, 0.0082, 0.0048, 0.1115, 0.0302, 0.1105, 0.0755, 0.0598, 0.0387, 0.1002, 0.0268, 0.0955, 0.0193, 0.0217, 0.1288},
+  {0.1157, 0.0047, 0.0648, 0.1104, 0.1164, 0.0291, 0.1021, 0.0909, 0.0444, 0.0101, 0.1222, 0.0665, 0.0205, 0.0235, 0.0242, 0.0545},
+  {0.1040, 0.0415, 0.0123, 0.0681, 0.0834, 0.0775, 0.1278, 0.0103, 0.0729, 0.0401, 0.0045, 0.0707, 0.0763, 0.1131, 0.0171, 0.0804}
 };
 
 // The probabilities for determining the very first state.
 // Corresponds to Racket's `initial-state-probabilities`.
-const float initial_state_probabilities[NUM_STATES] = {0.2, 0.2, 0.2, 0.2, 0.2};
+const float initial_state_probabilities[NUM_STATES] = {0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625, 0.0625};
 
 // Variable to hold the current state of the system (0 to NUM_STATES-1).
 int current_state;
@@ -178,7 +189,7 @@ void loop() {
   current_state = get_next_state(current_state);
   //delay(10);
 
-  for(int diode_id=0; diode_id < 5; diode_id++){
+  for(int diode_id=0; diode_id < NUM_STATES; diode_id++){
     if(diode_id == current_state) {
       drawDiode(diode_id, main_color);
     } else {
@@ -191,25 +202,58 @@ void loop() {
 
 void drawDiode(int id, uint16_t main_color) {
   uint16_t color;
-  for(uint32_t radius=1; radius<17; radius++){
+  for(uint32_t radius=1; radius<9; radius++){
     if(main_color != 0x0000)
       color = adjustColor(main_color);
 
     switch(id){
       case 0:
-        tft.drawSmoothCircle(TFT_HEIGHT/4, TFT_WIDTH/4, radius, color, color);
+        tft.drawSmoothCircle(1*TFT_HEIGHT/5, 1*TFT_WIDTH/5, radius, color, color);
         break;
       case 1:
-        tft.drawSmoothCircle(3*TFT_HEIGHT/4, TFT_WIDTH/4, radius, color, color);
+        tft.drawSmoothCircle(2*TFT_HEIGHT/5, 1*TFT_WIDTH/5, radius, color, color);
         break;
       case 2:
-        tft.drawSmoothCircle(TFT_HEIGHT/4, 3*TFT_WIDTH/4, radius, color, color);
+        tft.drawSmoothCircle(3*TFT_HEIGHT/5, 1*TFT_WIDTH/5, radius, color, color);
         break;
       case 3:
-        tft.drawSmoothCircle(3*TFT_HEIGHT/4, 3*TFT_WIDTH/4, radius, color, color);
+        tft.drawSmoothCircle(4*TFT_HEIGHT/5, 1*TFT_WIDTH/5, radius, color, color);
         break;
       case 4:
-        tft.drawSmoothCircle(TFT_HEIGHT/2, TFT_WIDTH/2, radius, color, color);
+        tft.drawSmoothCircle(1*TFT_HEIGHT/5, 2*TFT_WIDTH/5, radius, color, color);
+        break;
+      case 5:
+        tft.drawSmoothCircle(2*TFT_HEIGHT/5, 2*TFT_WIDTH/5, radius, color, color);
+        break;
+      case 6:
+        tft.drawSmoothCircle(3*TFT_HEIGHT/5, 2*TFT_WIDTH/5, radius, color, color);
+        break;
+      case 7:
+        tft.drawSmoothCircle(4*TFT_HEIGHT/5, 2*TFT_WIDTH/5, radius, color, color);
+        break;
+      case 8:
+        tft.drawSmoothCircle(1*TFT_HEIGHT/5, 3*TFT_WIDTH/5, radius, color, color);
+        break;
+      case 9:
+        tft.drawSmoothCircle(2*TFT_HEIGHT/5, 3*TFT_WIDTH/5, radius, color, color);
+        break;
+      case 10:
+        tft.drawSmoothCircle(3*TFT_HEIGHT/5, 3*TFT_WIDTH/5, radius, color, color);
+        break;
+      case 11:
+        tft.drawSmoothCircle(4*TFT_HEIGHT/5, 3*TFT_WIDTH/5, radius, color, color);
+        break;
+      case 12:
+        tft.drawSmoothCircle(1*TFT_HEIGHT/5, 4*TFT_WIDTH/5, radius, color, color);
+        break;
+      case 13:
+        tft.drawSmoothCircle(2*TFT_HEIGHT/5, 4*TFT_WIDTH/5, radius, color, color);
+        break;
+      case 14:
+        tft.drawSmoothCircle(3*TFT_HEIGHT/5, 4*TFT_WIDTH/5, radius, color, color);
+        break;
+      case 15:
+        tft.drawSmoothCircle(4*TFT_HEIGHT/5, 4*TFT_WIDTH/5, radius, color, color);
         break;
       default:
         break;
